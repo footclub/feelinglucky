e5b18cec6010cd8898b0788c06a453522ad71a83
footclub <61795237+footclub@users.noreply.github.com>
2024-06-07 19:59:03 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const slots = document.querySelectorAll('.slot');
 const spinButton = document.getElementById('spin');
 const resultText = document.getElementById('result');
 
-let currentValues = ['🍩',😀', '😃', '😄'];
+let currentValues = ['🍩',☕', '🧁', '🍔'];
 let previousValues = null;
 
 function spin() {
