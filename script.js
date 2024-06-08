a52cda9c31d4d3cea03ab5996a7777a608b6d5a8
footclub <61795237+footclub@users.noreply.github.com>
2024-06-08 16:33:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 const symbols = [ '🍋', '🍊', '🍇', '🍉','☕'];
-const winningCombination = ['☕', '☕', '☕']; // Define a winning combination
+const winner = '☕'
 
 function spin() {
     const slot1 = document.getElementById('slot1');
     const slot2 = document.getElementById('slot2');
     const slot3 = document.getElementById('slot3');
 
-    const symbol1 = getRandomSymbol();
-    const symbol2 = getRandomSymbol();
-    const symbol3 = getRandomSymbol();
+    slot1.classList.add('spin-animation');
+    slot2.classList.add('spin-animation');
+    slot3.classList.add('spin-animation');
 
-    slot1.innerText = symbol1;
-    slot2.innerText = symbol2;
-    slot3.innerText = symbol3;
+    setTimeout(() => {
+        const symbol1 = getRandomSymbol();
+        const symbol2 = getRandomSymbol();
+        const symbol3 = getRandomSymbol();
 
-    if (symbol1 === symbol2 && symbol2 === symbol3) {
-        displayWinMessage();
-    } else {
-        clearWinMessage();
-    }
+        slot1.innerText = symbol1;
+        slot2.innerText = symbol2;
+        slot3.innerText = symbol3;
+
+        slot1.classList.remove('spin-animation');
+        slot2.classList.remove('spin-animation');
+        slot3.classList.remove('spin-animation');
+
+        if (winner == symbol1 && winner == symbol2 && winner === symbol3) {
+            displayWinMessage();
+        } else {
+            clearWinMessage();
+        }
+    }, 2000); // Adjust the time for the spinning animation
 }
 
 function getRandomSymbol() {
